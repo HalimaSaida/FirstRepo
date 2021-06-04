f9c8ddf3d80e69c5f8448c92df832b1156451f2a
HalimaSaida <85364239+HalimaSaida@users.noreply.github.com>
2021-06-05 01:18:14 +0600

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,20 @@
+marks = input("Enter your mark:")
+marks =int(marks)
+
+if marks>= 80:
+   grade = "A+"
+
+elif marks>= 70:
+     grade = "A"
+
+elif marks>= 60:
+     grade = "A-"
+
+elif marks>= 50:
+     grade = "B"
+
+else:
+     grade= "F"
+
+print("Your grade is", grade)
+
